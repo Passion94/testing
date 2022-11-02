1e7210b36412aa9bc572289ce4c8d6ff901cf734
Passion94 <95107314+Passion94@users.noreply.github.com>
2022-11-02 03:24:47 -0700

    Second phase of the testing project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import {FaCalendar,FaTimes, FaGrav} from "react-icons/fa";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSuitcase } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faAsterisk } from "@fortawesome/free-solid-svg-icons";
+import { faGlobe } from "@fortawesome/free-solid-svg-icons";
 import './App.css';
+
+
+
+
 
 
 function App() {
   return (
     <div >
-    <h1><FaCalendar/> Favourite plans in life</h1>
+    <h1> <FontAwesomeIcon className="fa-icon" icon={faHome} /> Favourite plans in life</h1>
       <ul>
-      <li><FaTimes/> Learn Software development</li>
-      <li> <FaGrav/>Own a business</li>
-      <li> <FaTimes/>Fight Inequality in the soceity</li>
-      <li><FaGrav/> Provide access to sustainable skills for the upcoming generation</li>
-      <li> <FaTimes/>Become a link between man and God</li>
-      <li><FaGrav/> Get a life of my own</li>
+      <li> <FontAwesomeIcon className="fa-icon" icon={faSuitcase} /> Learn Software development</li>
+      <li> <FontAwesomeIcon className="fa-icon" icon={faEnvelope} /> Own a business</li>
+      <li> <FontAwesomeIcon className="fa-icon" icon={faPhone} />Fight Inequality in the soceity</li>
+      <li> <FontAwesomeIcon className="fa-icon" icon={faAsterisk} /> Provide access to sustainable skills for the upcoming generation</li>
+      <li> <FontAwesomeIcon className="fa-icon" icon={faGlobe} /> Become a link between man and God</li>
+      <li> <FontAwesomeIcon className="fa-icon" icon={faSuitcase} /> Get a life of my own</li>
        
        
       </ul>
